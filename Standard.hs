160787810f747a7e9896281dc0e4667fcf2b6e96
Liisi Kerik <liisikerik@hotmail.com>
2017-09-04 17:01:34 +0300

    -

--- Standard.hs
+++ Standard.hs
@@ -1,11 +1,4 @@
 -----------------------------------------------------------------------------------------------------------------------------
 {-# OPTIONS_GHC -Wall #-}
-module Typing where
-  ice :: t
-  ice = error "Internal compiler error"
-{-
-    "Matching error" ++
-    location' l c ++
-    ". Pattern name is not applicable to this algebraic data type or has already been matched."
--}
+module Standard where
 -----------------------------------------------------------------------------------------------------------------------------
